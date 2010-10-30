5f3a6af054715e8a3a6ebcf88ced2cdd76b84bb2
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2010-10-30 20:30:24 +0100

    Moved the helper methods into a seperate file so that the main lib file just loads the others.

--- init.rb
+++ init.rb
@@ -1,2 +1,1 @@
 require File.join(File.dirname(__FILE__), "lib", "js_namespace_framework")
-ActionView::Base.send :include, Js::Namespace::Framework
---
+++ lib/js_namespace_framework/helper_methods.rb
@@ -0,0 +1,35 @@
+module Js
+  module Namespace
+    module Framework
+      
+      def init_javascript
+        unless defined? SITE_NAME
+          raise Js::Namespace::Framework::Setup::Error, 'Please define SITE_NAME'
+        end
+        
+        action = params[:action]
+        action = 'new' if params[:action].eql? 'create'
+        action = 'edit' if params[:action].eql? 'update'
+        
+        javascript_tag("#{SITE_NAME}.#{params[:controller]}.#{action}_page();")
+      end
+      
+      def requires_javascript?
+        false
+      end
+      
+      def action_is?(*args)
+        return true if args.include? params[:action]
+        
+        return true if args.include? 'new' and params[:action].eql? 'create'
+        return true if args.include? 'edit' and params[:action].eql? 'update'
+        
+        return false
+      end
+      
+      module Setup
+        class Error < RuntimeError; end
+      end
+    end
+  end
+end
